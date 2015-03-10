8382756b6056858c5199546ae06638edce6bbacf
Md. Mesbahul Islam <mdislam@cs.helsinki.fi>
2015-03-10 17:22:25 +0200

    loop recursion complete

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -21,17 +21,47 @@
         :else (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [my-seq a-seq
+  	     index 0]
+  	(cond
+  		(empty? my-seq) nil
+  		(pred (first my-seq)) index
+  		:else (recur (rest my-seq) (inc index)))))
 
 (defn avg [a-seq]
-  -1)
+  (loop [sum 0
+         my-seq a-seq
+         num-elem (count a-seq)]
+    (cond (= num-elem 0) sum
+          (empty? my-seq) (/ sum num-elem)
+          :else (recur (+ sum (first my-seq)) (rest my-seq) num-elem))))
+
+
+(defn toggle [a-set elem]
+  (if (contains? a-set elem) (disj a-set elem) (conj a-set elem)))
 
 (defn parity [a-seq]
-  ":(")
+  (loop [my-seq a-seq
+         my-set (set [])]
+    (cond
+     (empty? my-seq) my-set
+     :else (recur (rest my-seq) (toggle my-set (first my-seq))))))
 
 (defn fast-fibo [n]
-  ":(")
+  (loop [second-num 1
+         first-num 0
+         third-num 2
+         n n]
+    (cond
+     (< n 2) n
+     (= third-num n) (+ second-num first-num)
+     :else (recur (+ second-num first-num) second-num (+ third-num 1) n))))
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+  (loop [k []
+  	     my-seq a-seq]
+  	(cond
+  		(empty? my-seq) k
+  		(contains? (set k) (first my-seq)) k
+  		:else (recur (conj k (first my-seq)) (rest my-seq)))))
 
