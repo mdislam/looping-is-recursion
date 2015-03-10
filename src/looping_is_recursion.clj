29d6c1194b9e113b79ed4cb3af2759dcd950bfe7
Md. Mesbahul Islam <mdislam@cs.helsinki.fi>
2015-03-10 16:08:18 +0200

    exercise done upto 3

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,13 +1,24 @@
 (ns looping-is-recursion)
 
 (defn power [base exp]
-  ":(")
+  (let [helper (fn[acc n]
+                 (if (zero? n)
+                   acc
+                   (recur (* acc base) (dec n))))]
+    (helper 1 exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [acc n]
+                 (if (empty? acc)
+                   n
+                   (recur (rest acc) (first acc))))]
+        (helper (rest a-seq) (first  a-seq))))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (cond (and (empty? seq1) (empty? seq2)) true
+        (or (empty? seq1) (empty? seq2)) false
+        (not= (first seq1) (first seq2)) false
+        :else (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
   ":(")
